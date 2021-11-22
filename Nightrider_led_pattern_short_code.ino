e35e7736bccbefcb4699805684ad72fd3a702e03
Malith Sankalpa Udawaththa <75824407+sankalpa-udawaththa@users.noreply.github.com>
2021-11-23 00:04:48 +0530

    Updated code

--- Nightrider_led_pattern_short_code.ino
+++ Nightrider_led_pattern_short_code.ino
@@ -1,15 +1,26 @@
-void setup (){
-    pinMode(1,OUTPUT);
-    pinMode(2,OUTPUT);
-    pinMode(3,OUTPUT);
-    pinMode(4,OUTPUT);
-    pinMode(5,OUTPUT);
-    pinMode(6,OUTPUT);
-    pinMode(13,OUTPUT);
+void setup() {
+pinMode(1,OUTPUT);
+pinMode(2,OUTPUT);
+pinMode(3,OUTPUT);
+pinMode(4,OUTPUT);
+pinMode(5,OUTPUT);
+pinMode(6,OUTPUT);
+ 
 }
-void loop(){
-digitalWrite(13,HIGH);
-delay(100);
-digitalWrite(13,LOW);
-delay(100);
+
+void loop() {
+ int t = 100;
+ 
+  for (int i = 1; i<7; i++){
+    digitalWrite(i,HIGH);
+    delay(t);
+    digitalWrite(i,LOW);
+    
+  }
+for (int l = 5; l>1; l--) {
+   digitalWrite(l,HIGH);
+    delay(t);
+    digitalWrite(l,LOW);
+}
+
 }
